7ac82eaf1c03f31b6aab3a10c40b5d086a7d7ebe
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-06-16 16:02:39 +0530

    form logic

--- Day 8/src/App.jsx
+++ Day 8/src/App.jsx
@@ -1,5 +1,4 @@
 import Faq from "./component/Faq";
-import Testimonial from "./component/testimonial";
 import Singuppage from "./pages/Singuppage";
 import NewTestimonial from "./component/NewTestimonial";
 
